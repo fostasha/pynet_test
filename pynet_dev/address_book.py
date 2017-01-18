d2a649b3f9804f182ab0db6f3e7dc56016dc84ac
Applied Python user <fostasha@vaulterxc@gmail.com>
2017-01-18 10:36:00 -0800

    test added pynet_dev/address_book.py

---
+++ pynet_dev/address_book.py
@@ -0,0 +1,11 @@
+ip_addr = ['10','0','20','1']
+ip_addr2 = ['10','0','21','1']
+ip_addr3 = ['10','0','22','1']
+
+
+address_book = [[ip_addr], [ip_addr2], [ip_addr3]]
+
+for list in address_book:
+    for x in list:
+        print x
+
